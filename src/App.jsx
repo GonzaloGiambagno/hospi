1dee71405d93b0f252fa283f98589a9a62d2c259
Gonzalo Giambagno <giambagno.gonzalo@gmail.com>
2023-06-28 22:03:07 -0300

    Config proyecto e inicio del navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import StickyNavbar from "./Components/layouts/StickyNavbar";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <StickyNavbar />
+
+      </div>
+    </>
+  )
+}
+
+export default App
