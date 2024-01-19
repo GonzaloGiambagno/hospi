2badab7feb26ad40971677ac8e4bae107f18d60e
Gonzalo Giambagno <giambagno.gonzalo@gmail.com>
2024-01-18 21:27:20 -0300

    vista login modificada

--- Client/src/Pages/Administracion/Login.jsx
+++ Client/src/Pages/Administracion/Login.jsx
@@ -1,15 +1,6 @@
+import LoginComponent from "../../Components/Admin/LoginComponent";
 import OpacityAnimated from "../../helpers/OpacityAnimated";
 import { Helmet } from "react-helmet";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-  Input,
-  Checkbox,
-  Button,
-} from "@material-tailwind/react";
 
 const Login = () => {
   return (
@@ -22,35 +13,7 @@ const Login = () => {
         />
       </Helmet>
       <div className="flex items-center justify-center h-screen bg-gray-100">
-        <Card className="w-96">
-          <CardHeader
-            variant="gradient"
-            color="green"
-            className="mb-2 grid h-24 place-items-center"
-          >
-            <Typography variant="h4" color="white">
-              Hospital italiano la plata
-            </Typography>
-          </CardHeader>
-          <CardBody className="flex flex-col gap-4">
-            <Input label="Email" size="lg" />
-            <Input label="Password" size="lg" />
-            <div className="-ml-2.5">
-              <Checkbox label="Remember Me" />
-            </div>
-          </CardBody>
-          <CardFooter className="pt-0">
-            <Button variant="gradient" color="green" fullWidth>
-              Ingresar
-            </Button>
-            <Typography
-              variant="small"
-              className="mt-6 flex justify-center italic"
-            >
-              Si tenes problemas para ingresar comunicate con Sistemas
-            </Typography>
-          </CardFooter>
-        </Card>
+        <LoginComponent />
       </div>
     </OpacityAnimated>
   );
